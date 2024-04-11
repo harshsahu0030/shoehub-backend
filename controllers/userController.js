cb704ae200ccb71297e0d3e6a5902d55c2accd3c
Harsh <harsh0030sahu@gmail.com>
2024-04-11 17:29:07 +0530

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -118,7 +118,7 @@ export const loginUserController = catchAsyncErrors(async (req, res, next) => {
   // options for cookie
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
+    secure: true,
   };
 
   res
